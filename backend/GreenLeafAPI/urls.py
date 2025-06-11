5eb475dadc513e163ab5b8bc660935e4a8798646
Yamlak Negash <164669390+yamneg96@users.noreply.github.com>
2025-06-11 13:23:45 +0300

    Update urls.py

--- backend/GreenLeafAPI/urls.py
+++ backend/GreenLeafAPI/urls.py
@@ -1,16 +1,3 @@
-# For generic views
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('plants/', views.PlantListCreateView.as_view(), name='plant-list-create'),
-#     path('plants/<int:pk>/', views.PlantRetrieveUpdateDestroyAPIView.as_view(), name='plant-detail'),
-
-#     path('observations/', views.ObservationListCreateAPIView.as_view(), name='observation-list-create'),
-#     path('observations/<int:pk>/', views.ObservationRetrieveUpdateDestroyAPIView.as_view(), name='observation-detail'),
-# ]
-
-# For ModelViewSet
 from rest_framework.routers import DefaultRouter
 from .views import PlantViewSet, ObservationViewSet
 
