b91089e0213194515198a148fc0e3ca333b381d3
Eyerusalem T/brhan <167539399+eyerus12345@users.noreply.github.com>
2025-06-11 12:57:52 +0300

    Update local_data_provider.dart

--- frontend/lib/application/providers/local_data_provider.dart
+++ frontend/lib/application/providers/local_data_provider.dart
@@ -1,1 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../infrastructure/local_data_source.dart';
 
+final localDataSourceProvider = Provider<LocalDataSource>((ref) {
+  return LocalDataSource();
+}); 
